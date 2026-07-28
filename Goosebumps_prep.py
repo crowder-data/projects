758e5fc0f6741f07f2a2e080cb53a77ebfa75628
crowder-data <ncrowder.itda@gmail.com>
2026-07-28 16:23:29 -0500

    Update Goosebumps_prep.py

--- Goosebumps_prep.py
+++ Goosebumps_prep.py
@@ -5,10 +5,13 @@ import matplotlib.pyplot as plt
 
 bins = [0,10,20,30,40,50,60,70,80,90,100,np.inf]
 labels = ['0-10','10-20','20-30','30-40','40-50','50-60','60-70','70-80','80-90','90-100','100+']
+filename = '/data/Goosebumps.csv'
 
-df_wide = pd.read_csv('/data/Goosebumps.csv').drop(columns = 'Total')
-df_long = df_wide.melt(id_vars=['First','Last'], var_name='Year',value_name='Miles')
-df_long['Range'] = pd.cut(df_long['Miles'],bins=bins,labels=labels)
+def load_data(filename=filename):
+  df_wide = pd.read_csv(filename).drop(columns = 'Total')
+  df_long = df_wide.melt(id_vars=['First','Last'], var_name='Year',value_name='Miles')
+  df_long['Range'] = pd.cut(df_long['Miles'],bins=bins,labels=labels)
+  return df_long
 
 def yearly_histogram(df, year, bins):
   data = df.loc[df['Year']==year, "Miles"]
