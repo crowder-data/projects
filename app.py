937587009f46997035cf5a82b1ccc0f629dcf39d
crowder-data <ncrowder.itda@gmail.com>
2026-07-28 16:26:46 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,39 @@
+import streamlit as st
+
+from goosebumps import (
+    load_data,
+    create_heatmap_data,
+    create_percent_heatmap_data,
+    yearly_histogram,
+    heatmap_raw,
+    heatmap_pct
+)
+
+
+df_long = load_data("Goosebumps.csv")
+
+heatmap_data = create_heatmap_data(df_long)
+heatmap_data_pct = create_percent_heatmap_data(heatmap_data)
+
+
+view = st.radio(
+    "View",
+    ["Yearly Histograms", "Raw Heatmap", "Percent Heatmap"]
+)
+
+
+if view == "Yearly Histograms":
+    year = st.selectbox(
+        "Year",
+        sorted(df_long["Year"].unique())
+    )
+
+    st.pyplot(yearly_histogram(df_long, year))
+
+
+elif view == "Raw Heatmap":
+    st.pyplot(heatmap_raw(heatmap_data))
+
+
+elif view == "Percent Heatmap":
+    st.pyplot(heatmap_pct(heatmap_data_pct))
