240160e179dbfece1d1c8822a94b77489796ccc1
crowder-data <ncrowder.itda@gmail.com>
2026-09-10 13:01:46 -0500

    Refactor data loading and visualization functions

--- goosebumps.py
+++ goosebumps.py
@@ -3,31 +3,28 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-
-
 def load_data(filename):
+  
   bins = [0,10,20,30,40,50,60,70,80,90,100,np.inf]
   labels = ['0-10','10-20','20-30','30-40','40-50','50-60','60-70','70-80','80-90','90-100','100+']
   df_wide = pd.read_csv(filename).drop(columns = 'Total')
   df_long = df_wide.melt(id_vars=['First','Last'], var_name='Year',value_name='Miles')
   df_long['Range'] = pd.cut(df_long['Miles'],bins=bins,labels=labels)
+  
   return df_long
 
 def yearly_histogram(df, year):
+  
   data = df.loc[df['Year']==year, "Miles"]
-
   fig, ax = plt.subplots(figsize = (8,4))
-
   ax.hist(
     data,
     bins=[0,10,20,30,40,50,60,70,80,90,100,np.inf],
     edgecolor='black'
   )
-
   ax.set_title(f'Runner Mileage Distribution - {year}')
   ax.set_xlabel('Total Miles')
   ax.set_ylabel('Runner Count')
-
   fig.tight_layout()
 
   return fig
@@ -48,8 +45,9 @@ def create_heatmap_data_percents(heatmap_data):
   return heatmap_data_percents
 
 def heatmap_raw(data):
-  fig, ax = plt.subplots(figsize = (16,8))
-    
+  
+  fig, ax = plt.subplots(figsize = (16,8))  
+  
   sns.heatmap(
     data,
     annot=True, 
@@ -74,15 +72,14 @@ def heatmap_raw(data):
 
   ax.collections[0].colorbar.set_label("Runner Count",fontsize=14)
   ax.collections[0].colorbar.ax.tick_params(labelsize=14)
-
   ax.set_xlabel("Year", fontsize=16)
   ax.set_ylabel("Mileage Range", fontsize=16)
-
   fig.tight_layout()
 
   return fig
 
 def heatmap_pct(data):
+  
   fig, ax = plt.subplots(figsize = (16,8))
   
   sns.heatmap(
@@ -93,7 +90,6 @@ def heatmap_pct(data):
       cmap = "Blues",
       annot_kws = {"fontsize":14}
   )
-
   
   ax.tick_params(
       axis="x",
@@ -109,10 +105,8 @@ def heatmap_pct(data):
 
   ax.collections[0].colorbar.set_label("Percent of Runners",fontsize=14)
   ax.collections[0].colorbar.ax.tick_params(labelsize=14)
-  
   ax.set_xlabel("Year", fontsize=16)
   ax.set_ylabel("Mileage Range", fontsize=16)
-
   fig.tight_layout()
   
   return fig
